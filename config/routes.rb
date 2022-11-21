7f144ad4f7ee2cfe9cdd1e259c402072a3e77561
A24-N <117143210+A24-N@users.noreply.github.com>
2022-11-21 11:52:47 +0000

    [save]1121

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/index'
-  get 'users/edit'
   devise_for :users
   root to: "homes#top"
   get 'about' => "homes#about", as:"about"
 
 
-  resources :books, only: [:index, :show, :edit, :create]
-  resources :users, only: [:index, :shoe, :edit, :destroy]
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
+  resources :users, only: [:index, :show, :edit, :destroy, :update]
 
 end
